9af1a698f3edc64a53791dd39184eb0bd047e7f2
s-dhiraj <dhiraj999saini@gmail.com>
2021-07-23 10:01:19 +0530

    second commit

--- controllers/middlewares/auth.js
+++
@@ -1,8 +0,0 @@
-function auth(req,res,next){
-    // passport provides method to check whether user is loggedin or not with req.isAuthenticated()
-    if(req.isAuthenticated()){
-        return next();
-    }
-   return  res.redirect("/login");
-}
-module.exports=auth;
